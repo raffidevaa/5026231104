671033fb8a060464de56c4efc3a2d7235f422d3b
raffidevaa <raffidevaa@gmail.com>
2025-05-21 12:36:46 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -49,3 +51,15 @@ Route::get('home', function () {
     return view('home'); //v
 });
 
+Route::get('dosen', [Link::class, 'index']);
+
+Route::get('pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+//Route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
